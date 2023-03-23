9b350912185d318443132172a7a63217e85f0cbe
davidfb93 <david.banol.3889@eam.edu.co>
2023-03-23 14:52:50 -0500

    Primer commit

---
+++ src/blackjack/usecases/index.js
@@ -0,0 +1,9 @@
+/* import { crearDeck as CrearNuevoDeck } from './usecases/crear-deck'; */
+/* import crearDeck from './usecases/crear-deck' */
+
+export { crearCartaHTML } from './crear-carta-html';
+export { crearDeck } from './crear-deck';
+export { pedirCarta } from './pedir-carta';
+export { turnoComputadora } from './turno-computadora';
+export { valorCarta } from './valor-carta';
+
